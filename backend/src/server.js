9daa9ec02aaca7132a42b528484d4b21ddabc3da
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-11-23 22:47:59 +0530

    update server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+
 import 'dotenv/config';
 import DBConnection from './config/dbConfig.js';
 
 const app = express();
+app.use(express.json());
+
+const userRoute = require('./routes/userRoute.js');
+
+
 
 const PORT = process.env.PORT || 8000;
 
 DBConnection();
+
+//Routes
+app.use('/user', userRoute);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server runing Port is ${PORT} `)
