14911c4c8b6c584bb369d6fbd64bb96bb6806636
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-12-01 20:09:25 +0530

    update & bug fix

--- frontend/src/routers/MainRouter.js
+++ frontend/src/routers/MainRouter.js
@@ -1,33 +1,69 @@
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Main from '../pages/Main';
 import Login from '../pages/Login';
+import Explore from '../pages/Explore';
+import Notification from '../pages/Notification';
+import Messeage from '../pages/Messeage';
+import Bookmarks from '../pages/Bookmarks';
+import List from '../pages/List';
+import UserProtected from './UserProtected';
+import Post from '../pages/Post';
+import Register from '../pages/Register';
 
-  
-const router = createBrowserRouter ([
-    {
-        path: "/",
-        element: <Main />,
-        children: [
-            {
-                index: true,
-                element: <Home />
-            },
-            {
-                path: "profile",
-                element: <Profile />
-            }
-        ]
-    },
-    {
-        path:"login",
-        element: <Login />
-    },
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserProtected />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/explore",
+        element: <Explore />
+      },
+      {
+        path: "/notification",
+        element: <Notification />
+      },
+      {
+        path: "/Messeage",
+        element: <Messeage />
+      },
+      {
+        path: "/bookmarks",
+        element: <Bookmarks />
+      },
+      {
+        path: "/list",
+        element: <List />
+      },
+      {
+        path: `/profile/:id`,
+        element: <Profile />,
+      },
+      {
+        path: '/:userName/status/:id',
+        element: <Post />
+      }
+    ]
+  },
+  {
+    path: "login",
+    element: <Login />
+  },
+  {
+    path: "register",
+    element: <Register />
+  }
 ]);
 
 const MainRouter = () => {
-  return <RouterProvider router={router} />
-}
-
-export default MainRouter
+  
+    return <RouterProvider router={router} />;
+  };
+  
+  export default MainRouter;
+  
