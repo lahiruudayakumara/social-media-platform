4fc9f937c7b6a6f65074915609427aa4310c8c07
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-12-03 17:37:40 +0530

    add auth controller

---
+++ backend/src/api/controllers/authController.js
@@ -0,0 +1,82 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+/* Register New User */
+const register = async (req, res) => {
+    try {
+        const {
+            name,
+            userName,
+            email,
+            password,
+            profilePicture,
+            coverPicture,
+            location,
+            birthday,
+            website,
+            subscribe,
+            viewedProfile,
+            impressions,
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            name,
+            userName,
+            email,
+            password: hashedPassword,
+            profilePicture,
+            coverPicture,
+            location,
+            birthday,
+            website,
+            subscribe,
+            viewedProfile,
+            impressions,
+        });
+        
+        const user = await newUser.save();
+        const userCopy = {...user.toObject()}
+        delete userCopy.password;
+        res.status(200).json({msg: "sucess", user: userCopy});
+
+    } catch (error) {
+        res.status(400).send({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+/* Login User */
+const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email: email});
+        if (!user) {
+            return res.status(400).json({msg :"User does not exists"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({msg :"Invalid Creditional"});
+        }
+        
+        const userCopy = {...user.toObject()}
+        delete userCopy.password;
+        const token = jwt.sign(userCopy, process.env.JWT_TOKEN);
+        res.status(200).json({token, userCopy });
+
+    } catch(error) {
+        res.status(500).json(error);
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
